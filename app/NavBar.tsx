f99c12ed378ef9530a672891d5c752919340f635
Mahtab <mahtaab.babaei@gmail.com>
2024-07-28 10:11:38 +0430

    Change the layout of the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { AiFillBug } from "react-icons/ai";
 import { useSession } from "next-auth/react";
-import { Box } from "@radix-ui/themes";
+import { Badge, Box, Container, Flex } from "@radix-ui/themes";
 
 const NavBar = () => {
   const currentPath = usePathname();
@@ -15,32 +15,55 @@ const NavBar = () => {
     { label: "Issues", href: "/issues/list" },
   ];
   return (
-    <nav className="flex border-b space-x-6 p-5 mb-5 items-center">
-      <Link href="/">
-        <AiFillBug className="text-amber-600" />
-      </Link>
-      {links.map((link) => (
-        <Link
-          key={link.href}
-          className={classnames({
-            "text-zinc-900": link.href === currentPath,
-            "text-zinc-500": link.href !== currentPath,
-            "hover:text-amber-600 transition-colors": true,
-          })}
-          href={link.href}
-        >
-          {link.label}
-        </Link>
-      ))}
-      {status === "unauthenticated" && (
-        <Box className="space-x-6">
-          <Link href="/auth/signup">Sign up</Link>
-          <Link href="/api/auth/signin">Login</Link>
-        </Box>
-      )}
-      {status === "authenticated" && (
-        <Link href="/api/auth/signout">Log out</Link>
-      )}
+    <nav className="border-b space-x-6 p-5 mb-5 items-center">
+      <Container>
+        <Flex justify="between">
+          <Flex gap="3" align="center">
+            <Link href="/">
+              <AiFillBug className="text-amber-600" />
+            </Link>
+            {links.map((link) => (
+              <Link
+                key={link.href}
+                className={classnames({
+                  "text-zinc-900": link.href === currentPath,
+                  "text-zinc-500": link.href !== currentPath,
+                  "hover:text-amber-600 transition-colors": true,
+                })}
+                href={link.href}
+              >
+                {link.label}
+              </Link>
+            ))}
+          </Flex>
+          <Box>
+            {status === "unauthenticated" && (
+              <Flex gap="3" align="center">
+                <Link
+                  href="/auth/signup"
+                  className="ring-none ring-inset bg-amber-50 rounded-lg p-1 text-amber-600 hover:ring-1 ring-amber-600 transition-all"
+                >
+                  Sign up
+                </Link>
+                <Link
+                  href="/api/auth/signin"
+                  className="text-zinc-500 hover:text-amber-600 transition-colors"
+                >
+                  Login
+                </Link>
+              </Flex>
+            )}
+            {status === "authenticated" && (
+              <Link
+                href="/api/auth/signout"
+                className="text-zinc-500 hover:text-amber-600 transition-colors"
+              >
+                Log out
+              </Link>
+            )}
+          </Box>
+        </Flex>
+      </Container>
     </nav>
   );
 };
