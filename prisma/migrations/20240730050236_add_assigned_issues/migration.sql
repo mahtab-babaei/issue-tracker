a85acd404ba2e5f671974d587e4d4e83a54dbb6b
Mahtab <mahtaab.babaei@gmail.com>
2024-08-06 10:48:06 +0430

    Delete the previous migrations and create a new migration

--- prisma/migrations/20240730050236_add_assigned_issues/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `issue` ADD COLUMN `userId` VARCHAR(191) NULL;
-
--- AddForeignKey
-ALTER TABLE `Issue` ADD CONSTRAINT `Issue_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
